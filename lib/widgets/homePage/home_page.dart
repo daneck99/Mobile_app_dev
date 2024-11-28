e625156a67da9639de96936e9cbbc9ffe2917efa
hj88nyyy <hjleeno1kr@gmail.com>
2024-11-28 12:37:51 +0900

    HomePage 추가

---
+++ lib/widgets/homePage/home_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:security/map/map_page.dart';
+import 'package:security/screens/taskList_page.dart';
+import 'package:security/screens/calendar_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    TaskListScreen(),
+    MapScreen(), // MapScreenPage로 이동
+    CalendarPage(),
+    Center(child: Text("프로필 화면")),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index; // 선택된 인덱스를 변경
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: '지도',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: '달력',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '프로필',
+          ),
+        ],
+      ),
+    );
+  }
+}
